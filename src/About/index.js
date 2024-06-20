2772cd5757c038b0d8631198cd39d0b3de3c94cc
Lavender <anyangolavenderr@gmail.com>
2024-06-20 13:52:07 +0300

    a[pis

---
+++ src/About/index.js
@@ -0,0 +1,12 @@
+import './index.css';
+
+
+function About(){
+    return(
+       <>
+        <h2>About Us </h2>
+        <p className="about-text">We are learning about ReactJs</p>
+       </>
+    )
+}
+export default About;
